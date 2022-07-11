b7dd36eb774d1eda14ad6b65d577bd818495f1c6
dzhaniivanov94 <68897146+dzhaniivanov94@users.noreply.github.com>
2022-07-11 15:19:43 +0300

    work on navbar,videocard components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import styled, { ThemeProvider } from "styled-components";
 import Menu from "./components/Menu";
 import Navbar from "./components/Navbar";
 import { darkTheme, lightTheme } from "./utils/Theme";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Video from "./pages/Video";
 
 const Container = styled.div`
   display: flex;
@@ -10,20 +13,33 @@ const Container = styled.div`
 
 const Main = styled.div`
   flex: 7;
-  background-attachment: ${({ theme }) => theme.bg};;
+  background-attachment: ${({ theme }) => theme.bg}; ;
 `;
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  padding: 22px 96px;
+`;
 
 function App() {
   const [darkMode, setDarkMode] = useState(true);
   return (
     <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
       <Container>
-        <Menu darkMode={darkMode} setDarkMode={setDarkMode}/>
-        <Main>
-          <Navbar />
-          <Wrapper>video cards</Wrapper>
-        </Main>
+        <BrowserRouter>
+          <Menu darkMode={darkMode} setDarkMode={setDarkMode} />
+          <Main>
+            <Navbar />
+            <Wrapper>
+              <Routes>
+                <Route path="/">
+                  <Route index element={<Home />} />
+                  <Route path="video">
+                    <Route path=":id" element={<Video />} />
+                  </Route>
+                </Route>
+              </Routes>
+            </Wrapper>
+          </Main>
+        </BrowserRouter>
       </Container>
     </ThemeProvider>
   );
---
+++ src/pages/Video.js
@@ -0,0 +1,170 @@
+import styled from "styled-components";
+import ThumbUpOutlinedIcon from "@mui/icons-material/ThumbUpOutlined";
+import ThumbDownOffAltOutlinedIcon from "@mui/icons-material/ThumbDownOffAltOutlined";
+import ReplyOutlinedIcon from "@mui/icons-material/ReplyOutlined";
+import AddTaskOutlinedIcon from "@mui/icons-material/AddTaskOutlined";
+
+const Container = styled.div`
+  display: flex;
+  gap: 24px;
+`;
+
+const VideoWrapper = styled.div``;
+
+const Title = styled.h1`
+  font-size: 18px;
+  font-weight: 400;
+  margin-top: 20px;
+  margin-bottom: 10px;
+  color: ${({ theme }) => theme.text};
+`;
+
+const Details = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Info = styled.span`
+  color: ${({ theme }) => theme.textSoft};
+`;
+
+const Buttons = styled.div`
+  display: flex;
+  gap: 20px;
+  color: ${({ theme }) => theme.text};
+`;
+
+const Button = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  cursor: pointer;
+`;
+
+const Hr = styled.hr`
+  margin: 15px 0px;
+  border: 0.5px solid ${({ theme }) => theme.soft};
+`;
+
+const Recommendation = styled.div`
+  flex: 2;
+`;
+
+const Channel = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const ChannelInfo = styled.div`
+  display: flex;
+  gap: 20px;
+`;
+
+const Subscribe = styled.button`
+  background-color: #cc1a00;
+  font-weight: 500;
+  color:white;
+  border:none;
+  border-radius:3px;
+  height:max-content;
+  cursor:pointer;
+  padding:10px 20px;
+`;
+
+const Image = styled.img`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+`;
+
+const ChannelDetail = styled.div`
+  display: flex;
+  flex-direction: column;
+  color: ${({ theme }) => theme.text};
+  font-size: 12px;
+`;
+
+const ChannelName = styled.span`
+  font-weight: 500;
+`;
+
+const ChannelCounter = styled.span`
+  margin-top: 5px;
+  margin-bottom: 20px;
+  color: ${({ theme }) => theme.textSoft};
+  font-size: 12px;
+`;  
+
+const Description = styled.p`
+  font-size: 14px;
+`;
+
+const Content = styled.div`
+  flex: 5;
+`;
+
+const Video = () => {
+  return (
+    <Container>
+      <Content>
+        <VideoWrapper>
+          <iframe
+            width="100%"
+            height="720"
+            src="https://www.youtube.com/embed/k3Vfj-e1Ma4"
+            title="YouTube video player"
+            frameborder="0"
+            allow="acceleroemter; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </VideoWrapper>
+        <Title>Test Video</Title>
+        <Details>
+          <Info>32233232 views / 11 Jul 2022</Info>
+          <Buttons>
+            <Button>
+              <ThumbUpOutlinedIcon />
+              123
+            </Button>
+            <Button>
+              <ThumbDownOffAltOutlinedIcon />
+              Dislike
+            </Button>
+            <Button>
+              <ReplyOutlinedIcon />
+              Share
+            </Button>
+            <Button>
+              <AddTaskOutlinedIcon />
+              Save
+            </Button>
+          </Buttons>
+        </Details>
+        <Hr />
+        <Channel>
+          <ChannelInfo>
+            <Image src="https://yt3.ggpht.com/yti/APfAmoE-Q0ZLJ4vk3vqmV4Kwp0sbrjxLyB8Q4ZgNsiRH=s88-c-k-c0x00ffffff-no-rj-mo" />
+            <ChannelDetail>
+              <ChannelName>Test Channel</ChannelName>
+              <ChannelCounter>200k subscribers</ChannelCounter>
+              <Description>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab
+                placeat aperiam quia perferendis. Praesentium, totam quaerat,
+                molestiae, quas architecto eligendi ipsam aperiam doloribus
+                laudantium nostrum sint optio quia voluptatum porro amet eum
+                officiis? Autem, ex? Iusto adipisci itaque possimus magnam ad
+                aspernatur eligendi, sapiente nesciunt nam quas corrupti eum
+                voluptates!
+              </Description>
+            </ChannelDetail>
+          </ChannelInfo>
+          <Subscribe>SUBSCRIBE</Subscribe>
+        </Channel>
+      </Content>
+      <Recommendation>recommendation</Recommendation>
+    </Container>
+  );
+};
+
+export default Video;
